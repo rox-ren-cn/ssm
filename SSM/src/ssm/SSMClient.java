a8a775a0c696834605746e81f40fce4f5f01a7fe
roxxette <roxxette@163.com>
2016-05-22 14:33:37 +0800

    Add files

---
+++ SSM/src/ssm/SSMClient.java
@@ -0,0 +1,91 @@
+package ssm;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.Socket;
+
+import javax.swing.JFrame;
+//import javax.swing.JOptionPane;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+
+public class SSMClient {
+
+	InputStream is;
+	OutputStream os;
+	private JFrame frame = new JFrame("Capitalize Client");
+	private JTextField dataField = new JTextField(40);
+	private JTextArea messageArea = new JTextArea(8, 60);
+
+	public SSMClient() {
+		messageArea.setEditable(false);
+		frame.getContentPane().add(dataField, "North");
+		frame.getContentPane().add(new JScrollPane(messageArea), "Center");
+
+		dataField.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				String msg = "";
+				try {
+					switch (dataField.getText()) {
+					case "1":
+						msg = "SSMIM12345678";
+						break;
+					case "2":
+						msg = "SSMXK012345678";
+						break;
+					case "3":
+						msg = "SSMXK112345678";
+						break;
+					case "4":
+						msg = "SSMTP123456788888888800000000000000000000000000000000";
+						break;
+					default:
+						msg = "SSMIM12345678";
+					}
+					os.write(msg.getBytes(), 0, 13);
+				} catch (IOException e1) {
+					e1.printStackTrace();
+				}
+				String response;
+				try {
+					byte[] b = new byte[100];
+					int len = is.read(b, 0, 100);
+					response = new String(b, 0, len);
+					if (response == null || response.equals("")) {
+						System.exit(0);
+					}
+				} catch (IOException ex) {
+					response = "Error: " + ex;
+				}
+				messageArea.append(response + "\n");
+				dataField.selectAll();
+			}
+		});
+	}
+
+	public void connectToServer() throws IOException {
+		// String serverAddress = JOptionPane.showInputDialog(
+		// frame,
+		// "Enter IP Address of the Server:",
+		// "Welcome to the Capitalization Program",
+		// JOptionPane.QUESTION_MESSAGE);
+		String serverAddress = "localhost";
+
+		@SuppressWarnings("resource")
+		Socket socket = new Socket(serverAddress, 12345);
+		is = socket.getInputStream();
+		os = socket.getOutputStream();
+	}
+
+	public static void main(String[] args) throws Exception {
+		SSMClient client = new SSMClient();
+		client.frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		client.frame.pack();
+		client.frame.setVisible(true);
+		client.connectToServer();
+	}
+}
---
+++ SSM/src/ssm/util/BadCmdException.java
@@ -0,0 +1,11 @@
+package ssm.util;
+
+public class BadCmdException extends Exception {
+
+	public BadCmdException(String string) {
+		super(string);
+	}
+
+	private static final long serialVersionUID = 1L;
+
+}
